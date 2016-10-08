5a09bc36044e094c3b9cf9ed1ea21acfb8edd86b
Drew Blasius <andrew.blasius@case.edu>
2016-10-08 17:53:53 -0400

    Fixed spacing issues

--- Graph.py
+++ Graph.py
@@ -2,86 +2,86 @@
 
 class Graph(object):
         # The graph object assumes that you pass in a adjacency vertex matrix
-        def __init__(self,adjList=None):
-                if adjList is None:
-                        self.setAdjList([])
-                else:
-                        self.setAdjList(adjList)
-                self.setVertexTable({})
-                sefl.setEdgeTable({})
+    def __init__(self,adjList=None):
+        if adjList is None:
+            self.setAdjList([])
+        else:
+            self.setAdjList(adjList)
+        self.setVertexTable({})
+        self.setEdgeTable({})
 
-        def setVertexTable(self,vertexTable):
-                self.__vertexTable = vertexTable
+    def setVertexTable(self,vertexTable):
+        self.__vertexTable = vertexTable
 
-        def getVertexTable(self):
-                return self.__vertexTable
+    def getVertexTable(self):
+        return self.__vertexTable
 
-        def setEdgeTable(self,edgeTable):
-                self.__edgeTable = edgeTable
+    def setEdgeTable(self,edgeTable):
+        self.__edgeTable = edgeTable
 
-        def getEdgeTable(self):
-                return self.__edgeTable
+    def getEdgeTable(self):
+        return self.__edgeTable
 
-        def setAdjList(self,adjList):
-                self.__adjList = adjList
+    def setAdjList(self,adjList):
+        self.__adjList = adjList
 
-        def getAdjList(self):
-                return self.__adjList
+    def getAdjList(self):
+        return self.__adjList
 
-        def adjacent(self,x,y):
-                return (y in self.getAdjList()[x])
+    def adjacent(self,x,y):
+        return (y in self.getAdjList()[x])
 
-        def neighbors(self,x):
-                return self.getAdjList()[x]
+    def neighbors(self,x):
+        return self.getAdjList()[x]
 
-        def addVertex(self,x):
-                if x not in self.getAdjList():
-                        self.getAdjList()[x] = []
+    def addVertex(self,x):
+        if x not in self.getAdjList():
+            self.getAdjList()[x] = []
 
-        def addEdge(self,x,y):
-                if y not in self.getAdjList()[x]:
-                        self.getAdjList()[x].append(y)
-                        self.getAdjList()[y].append(x)
+    def addEdge(self,x,y):
+        if y not in self.getAdjList()[x]:
+            self.getAdjList()[x].append(y)
+            self.getAdjList()[y].append(x)
 
-        def removeEdge(self,x,y):
-                if y in self.getAdjList()[x]:
-                        self.getAdjList()[x].remove(y)
-                        self.getAdjList()[y].remove(x)
+    def removeEdge(self,x,y):
+        if y in self.getAdjList()[x]:
+            self.getAdjList()[x].remove(y)
+            self.getAdjList()[y].remove(x)
 
-        def setVertexValue(self,x,value):
-                self.getVertexTable()[x] = value
+    def setVertexValue(self,x,value):
+        self.getVertexTable()[x] = value
 
-        def getVertexValue(self,x):
-                return self.getVertexTable()[x]
+    def getVertexValue(self,x):
+        return self.getVertexTable()[x]
 
-        def setEdgeValue(self,x,y,value):
-                self.getEdgeTable()[(x,y)] = value
+    def setEdgeValue(self,x,y,value):
+        self.getEdgeTable()[(x,y)] = value
 
-        def getEdgeValue(self,x,y):
-                return self.getEdgeTable()[(x,y)]
+    def getEdgeValue(self,x,y):
+        return self.getEdgeTable()[(x,y)]
 
 
 class DirectedGraph(Graph):
-        def addEdge(self,x,y):
-                if y not in self.getAdjList()[x]:
-                        self.getAdjList()[x].append(y)
+    def addEdge(self,x,y):
+        if y not in self.getAdjList()[x]:
+            self.getAdjList()[x].append(y)
 
-        def removeEdge(self,x,y):
-                if y in self.getAdjList()[x]:
-                        self.getAdjList()[x].remove(y)
+    def removeEdge(self,x,y):
+        if y in self.getAdjList()[x]:
+            self.getAdjList()[x].remove(y)
 
 
 class LayeredGraph(DirectedGraph):
-        def __init__(self,adjList=None):
-                # initializes the layer map
-                super().__init__(adjList)
-                self.setLayerMap([])
+    def __init__(self,adjList=None):
+        # initializes the layer map
+        super().__init__(adjList)
+        self.setLayerMap([])
 
-        def setLayerMap(self,layerMap):
-                self.__layerMap = layerMap
+    def setLayerMap(self,layerMap):
+        self.__layerMap = layerMap
         
-        def getLayerMap(self):
-                return self.__layerMap
+    def getLayerMap(self):
+        return self.__layerMap
 
 
 class NeuralNetwork(LayeredGraph):
@@ -93,27 +93,27 @@ class NeuralNetwork(LayeredGraph):
         nextVertexList = self.neighbors(vertex)
         output         = self.getVertexValue(vertex).getOutput()
         for nextVertex in nextVertexList:
-                self.getVertexValue(nextVertex).addToInput(output)
+            self.getVertexValue(nextVertex).addToInput(output)
 
     def setInputLayer(self,inputData):
         inputLayer = self.getLayerMap()[0]
         for i in range(len(inputLayer)):
-                vertex = inputLayer[i]
-                self.getVertexValue(vertex).setInput(inputData[i])
+            vertex = inputLayer[i]
+            self.getVertexValue(vertex).setInput(inputData[i])
 
     def compute(self,inputData):
         self.setInputLayer(inputData)                         # setting up input layer
         for vertexLayer in self.getLayerMap():
-                for vertex in vertexLayer:
-                        self.getVertexValue(vertex).compute() # each neuron does a computation
-                        self.pipe(vertex)                     # pipes data to next layer of neurons
+            for vertex in vertexLayer:
+                self.getVertexValue(vertex).compute() # each neuron does a computation
+                self.pipe(vertex)                     # pipes data to next layer of neurons
         
     def backpropagation(self,trainingData,learningRate):
         for [example,result] in trainingData:
-                self.compute(example)
-                self.getOutputError(result)
-                self.getHiddenError()
-                self.updateWeights(learningRate)
+            self.compute(example)
+            self.getOutputError(result)
+            self.getHiddenError()
+            self.updateWeights(learningRate)
 
     def getOutputError(self,example):
         outputLayer = self.getLayerMap()[-1]
